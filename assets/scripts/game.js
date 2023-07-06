bc27d3705ba8d1e086aaf33a50e1279076a10847
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2023-07-06 16:20:24 +0100

    Refactor code for readability

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,74 +1,82 @@
 const k = kaboom({
     width: 1280,
     height: 720,
+    background:[35, 35, 35]
 });
-
-k.scene('main', () => {
-    k.add([
-        
-    ]);
-
-    addLevel([
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "                                       ",
-        "         / = =                         ",
-        "        /  #                           ",
-        "       /  #  #                         ",
-        "         #  #                          ",
-        "= = = = = = = = =   = = = = = = = = = =",
-    ], {
-        tileWidth:32,
-        tileHeight:32,
-        tiles: {
-            "=": () => [
-                ground = sprite('tile-2'),
-                area(),
-                scale(2),
-            ],
-            "/": () => [
-                rampUp = sprite('tile-47'),
-                area(),
-                scale(2),
-            ],
-            "|": () => [
-                rampDown = sprite('tile-49'),
-                area(),
-                scale(2),
-            ],
-            "#": ()=>[
-                dirt = sprite('tile-12'),
-                area(),
-                scale(2),
-            ]
-        }
-    })
-    
-});
-
 // a function to load all 60 swamp tiles to avoid repetition
 function loadAllTiles(){
     let tilenumber = 0;
     for(let i=0; i < 60; i++){
         tilenumber++;
-        console.log(tilenumber);
-        loadSprite(`tile-${tilenumber}`, `assets/images/tiles/swamp/swamp-tile-${tilenumber}.png`);
+        loadSprite(`swampTile${tilenumber}`, `assets/images/tiles/swamp/swamp-tile-${tilenumber}.png`);
+        console.log("loadedtile: " + tilenumber);
     };
 };
 
 loadAllTiles();
+//creating a map constant for level1
+const map1 = [
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "         /=                            ",
+        "        /##                            ",
+        "       /###                            ",
+        "      /####                            ",
+        "===========   =========================",
+    ];
+
+//creating a level configuration constant 
+const levelConfig = {
+    tileWidth:32,
+    tileHeight:32,
+    tiles: {
+        "=": () => [
+            ground = sprite('swampTile2'),
+            area(),
+            scale(1),
+        ],
+        "/": () => [
+            rampUp = sprite('swampTile47'),
+            area(),
+            scale(1),
+        ],
+        "|": () => [
+            rampDown = sprite('swampTile49'),
+            area(),
+            scale(1),
+        ],
+        "#": ()=>[
+            dirt = sprite('swampTile12'),
+            area(),
+            scale(1),
+        ],
+
+    }
+}
+
+const player ={
+
+}
+
+    
+//main function to run the game
+k.scene('main', () => {
+    //add level function takes map and level config to build a level
+    addLevel(map1, levelConfig); 
+});
 
 k.go('main');
