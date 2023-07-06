451623d46303ccdf190ae74f45b9970a8f6bcf02
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2023-07-06 14:18:44 +0100

    Add function to batch load tiles

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,5 +1,3 @@
-
-
 const k = kaboom({
     width: 1280,
     height: 720,
@@ -11,11 +9,26 @@ k.scene('main', () => {
     ]);
 
     addLevel([
-        "                   ",
-        "                   ",
-        "                   ",
-        "                   ",
-        "                   ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "                                       ",
+        "         / = =                         ",
+        "        /  #                           ",
+        "       /  #  #                         ",
+        "         #  #                          ",
         "= = = = = = = = =   = = = = = = = = = =",
     ], {
         tileWidth:32,
@@ -25,25 +38,37 @@ k.scene('main', () => {
                 ground = sprite('tile-2'),
                 area(),
                 scale(2),
-                pos(0, 500)
             ],
+            "/": () => [
+                rampUp = sprite('tile-47'),
+                area(),
+                scale(2),
+            ],
+            "|": () => [
+                rampDown = sprite('tile-49'),
+                area(),
+                scale(2),
+            ],
+            "#": ()=>[
+                dirt = sprite('tile-12'),
+                area(),
+                scale(2),
+            ]
         }
     })
     
 });
+
 // a function to load all 60 swamp tiles to avoid repetition
- 
 function loadAllTiles(){
     let tilenumber = 0;
     for(let i=0; i < 60; i++){
         tilenumber++;
         console.log(tilenumber);
-        loadSprite(`tile-${tilenumber}`, `assets/images/tiles/tile-${tilenumber}.png`);
+        loadSprite(`tile-${tilenumber}`, `assets/images/tiles/swamp/swamp-tile-${tilenumber}.png`);
     };
 };
 
 loadAllTiles();
-
-
 
 k.go('main');
