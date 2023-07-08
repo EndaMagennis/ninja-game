02adb0057d7a2dc2b043ae2b9731c52af90a404f
Enda Magennis <101476399+RopesMcMeme@users.noreply.github.com>
2023-07-08 13:08:23 +0100

    Add camera function

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -10,7 +10,16 @@ const k = kaboom({
     background: [0, 0, 0, 0],
 });
 
-//loading background sprites
+//loading icons for mobile play
+loadSprite('leftArrow', 'assets/images/icons/arrow-left.png');
+
+loadSprite('rightArrow', 'assets/images/icons/arrow-right.png');
+
+loadSprite('button1', 'assets/images/icons/button1.png');
+
+loadSprite('button2', 'assets/images/icons/button2.png');
+
+loadSprite('menu', 'assets/images/icons/bars-horizontal.png');
 
 //loading sprites animations for use in game.
 loadSprite('playerIdle', 'assets/images/sprites/kunoichi/kunoichi-idle.png', {
@@ -37,24 +46,7 @@ loadSprite('playerFall', 'assets/images/sprites/kunoichi/kunoichi-jump.png', {
     sliceX: 10, sliceY: 1,
     anims: {'fallAnim': {from: 8, to: 9, loop: true}}
 })
-
-// a function to load all 60 swamp tiles to avoid repetition
-function loadAllTiles(){
-    let tilenumber = 0;
-    for(let i=0; i < 60; i++){
-        tilenumber++;
-        loadSprite(`swampTile${tilenumber}`, `assets/images/tiles/swamp/swamp-tile-${tilenumber}.png`);
-        console.log("loadedtile: " + tilenumber);
-    };
-};
-
-//calling the loadAllTiles function
-loadAllTiles();
-
-//setting gravity 
-setGravity(1000);
-
-//creating a map constant for level1
+//creating a map constant for level
 const map1 = [
     
         "                                       ",
@@ -78,11 +70,8 @@ const map1 = [
         "                                       ",
         "                                       ",
         "                                       ",
-        "                                       ",
-        "                                       ",
         "===========   =========================",
     ];
-
 //creating a constant for level configurations
 const levelConfig = {
     tileWidth:16,
@@ -111,6 +100,7 @@ const levelConfig = {
 
     }
 };
+
 //creates a player object
 const player = add([
     sprite('playerIdle'),//default animation
@@ -128,31 +118,99 @@ const player = add([
     },
     "player",
 ]);
-player.play('idleAnim');
+const leftButton = add([
+    sprite('leftArrow'),
+    pos(50, height() -50),
+    opacity(0.5),
+    fixed(),
+    area()
+])
+const rightButton = add([
+    sprite('rightArrow'),
+    pos(90, height() -50),
+    opacity(0.5),
+    fixed(),
+    area()
+])
+const button1 = add([
+    sprite('button1'),
+    pos(width() -160, height() -50),
+    opacity(0.5),
+    fixed(),
+    area()
+])
+const button2 = add([
+    sprite('button2'),
+    pos(width()-120, height() -50),
+    opacity(0.5),
+    fixed(),
+    area()
+])
+// a function to load all 60 swamp tiles to avoid repetition
+function loadAllTiles(){
+    let tilenumber = 0;
+    for(let i=0; i < 60; i++){
+        tilenumber++;
+        loadSprite(`swampTile${tilenumber}`, `assets/images/tiles/swamp/swamp-tile-${tilenumber}.png`);
+        console.log("loadedtile: " + tilenumber);
+    };
+};
 
+//calling the loadAllTiles function
+loadAllTiles();
+
+//setting gravity 
+setGravity(1000);
+
+
+player.play('idleAnim');
+handleInputs()
+camScale(1.5)
 //onUpdate is called every frame
 onUpdate(() =>{
+    cameraConfig();
+
     if(player.curAnim() !== 'runAnim' && player.isGrounded()){
         player.use(sprite('playerIdle'));
         player.play('idleAnim');
-    }
+    };
 
     if(player.curAnim() !== 'jumpAnim' && !player.isGrounded() && player.heightDelta > 0) {
         player.use(sprite('playerJump'));
         player.play('jumpAnim');
-    }
+    };
 
     if(player.curAnim() !== 'fallAnim' && !player.isGrounded() && player.heightDelta < 0) {
         player.use(sprite('playerFall'));
         player.play('fallAnim');
-    }
+    };
 
     if(player.direction === 'left'){
         player.flipX = true;
     } else{
         player.flipX = false;
-    }
+    };
 });
+
+function cameraConfig(){
+    if(player.previousHeight){
+        player.heightDelta = player.previousHeight - player.pos.y;
+    };
+
+    player.previousHeight = player.pos.y;
+
+    const cameraLeftBound = 100;
+    const camerRightBound = 4000;
+    const cameraVerticalOffset = player.pos.y -10;
+
+    if(cameraLeftBound > player.pos.x){
+        camPos(cameraLeftBound, cameraVerticalOffset);
+    } else if(camerRightBound < player.pos.x) {
+        camPos(camerRightBound, cameraVerticalOffset);
+    } else{
+        camPos(player.pos.x, cameraVerticalOffset);
+    }
+}
 
 function idle(){
         player.use(sprite('playerIdle'));
@@ -216,11 +274,31 @@ function handleInputs(){
         playerJump();
     })
 };
+
+function handleMobileInputs(){
+    onTouchStart((id,pos) =>{
+        if(leftButton.hasPoint(pos)){
+            moveLeft();
+            leftButton.opacity = 1;
+        } else if(rightButton.hasPoint(pos)){
+            moveRight();
+            rightButton.opacity = 1;
+        } else if(button1.hasPoint(pos)){
+            playerJump();
+        }
+    })
+
+    onTouchEnd((_, pos) => {
+        if(!leftButton.hasPoint(pos)){
+
+        }
+    })
+}
+
 //creating a level configuration constant 
 onCollide("player", "ground", ()=> {
         player.isGrounded();
         console.log("hasLanded");
 });
-handleInputs();
 addLevel(map1, levelConfig);
 
